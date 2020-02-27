1c57b5ab7560f1fc871ffad92c20be9e1e52df92
Luis4609 <luis.monzon.pedreira@alumnos.upm.es>
2020-02-27 20:50:16 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -10,6 +11,7 @@ import { ProductsAlertsComponent } from './product-alerts/product-alerts.compone
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
+
 
 @NgModule({
   imports: [
