e0d49b2a4e431e1ad34d89b978f6583661245623
Luis4609 <luis.monzon.pedreira@alumnos.upm.es>
2020-02-27 20:08:15 +0100

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -6,6 +6,18 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   items = [];
+    addToCart(product) {
+    this.items.push(product);   //Anade productos al array items
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];  //Devuelve el valor de items a vacio, para vaciar el cart
+    return this.items;
+  }
   constructor() {}
 
 }
